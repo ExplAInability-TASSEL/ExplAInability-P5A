802e67adb1de53d71384edbbef4a1bcb07532717
Thibault-GILLARD <117235512+Thibault-GILLARD@users.noreply.github.com>
2023-11-14 22:14:44 +0000

    Create the model class

--- main.py
+++ main.py
@@ -4,106 +4,22 @@ import numpy as np
 from py3.CNN_model import Cplx_CustomCNN_1D
 from py3.Attention_Layer import CustomAttentionLayer
 from py3.classification import CustomClassifierModel
-
-# Usage example, With an segment of 100 pixels, each with shape (73, 10)
-pixels = np.random.random((100, 73, 10))
-
-# we want to generate more realistic values for the pixels, so we help the clustering algorithm
-generator = PixelValueGenerator(low_value_percentage=0.8)
-generator.generate_values(pixels.shape)
-pixels = generator.get_values()
-
-# object of the class CustomKMeans
-n_clusters=2
-custom_kmeans = CustomKMeans(n_clusters=n_clusters)
-
-# Fit the K-Means model and retrieve cluster labels and centers
-custom_kmeans.fit(pixels)
-print("hey")
-# results
-cluster_labels = custom_kmeans.get_cluster_labels()
-cluster_centers = custom_kmeans.get_cluster_centers()
-print("cluster_labels shape ",cluster_labels.shape)
-print("Number of pixels in each cluster: ", np.bincount(cluster_labels))
-print("cluster_centers shape ",cluster_centers.shape)
-
-cluster_1 = cluster_centers[0]
-print(f'cluster_1 shape: {cluster_1.shape}')
-# Reshape cluster_1 to match the input shape of CustomCNN
-cluster_1 = cluster_1.reshape((1,) + cluster_1.shape)  # Add the batch dimension
-
-cluster_2 = cluster_centers[1]
-print(f'cluster_2 shape: {cluster_2.shape}')
-
-# Reshape cluster_2 to match the input shape of CustomCNN
-cluster_2 = cluster_2.reshape((1,) + cluster_2.shape)  # Add the batch dimension
- 
-# Create an instance of CustomCNN
-custom_cnn = Cplx_CustomCNN_1D(input_shape=cluster_2.shape[1:], num_classes=7)
-
-# Compile the model
-custom_cnn.compile_model()
- 
-# Summarize the model
-custom_cnn.summary()
- 
-#custom_cnn.model to make predictions
-predictions2 = custom_cnn.model.predict(cluster_2)
-
-# prediction 1 
-predictions1 = custom_cnn.model.predict(cluster_1)
-
-# Print the predictions
-print(predictions2)
-print(predictions1)
-print(f"predictions1 shape: {predictions1.shape}")
-print(f"predictions2 shape: {predictions2.shape}")
-
-# join predictions as in cluster_centers 
-predictions = np.concatenate((predictions1, predictions2), axis=0)
-
-print(predictions)
-print(f"predictions shape: {predictions.shape}")
-
-# Create an instance of CustomAttentionLayer
-attn_layer = CustomAttentionLayer(units=1)
-
-# Get the attention weights
-attn_weights = attn_layer(predictions)
- 
- 
-# Print the attention weights
-print(attn_weights)
-print(f"attn_weights shape: {attn_weights.shape}")
- 
-h = attn_layer.get_weighted_sum(predictions, attn_weights)
-print(h)
-print(f"h shape: {h.shape}")
-
-classifier = CustomClassifierModel(num_classes=8)
-classifier.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-classifier.summary()
- 
-# Make predictions
-predictions = classifier.predict(h)
-print(predictions)
-print(f"predictions shape: {predictions.shape}")
-
 import tensorflow as tf
  
 class MODEL():
     def __init__(self, num_classes=7, num_clusters=2):
         self.num_classes = num_classes
         self.num_clusters = num_clusters
-        self.cluster_centers = CustomKMeans(n_clusters=self.num_clusters) 
+        self.kmeans = CustomKMeans(n_clusters=self.num_clusters) 
         self.input_shape = self.calculate_input_shape()
-        self.enc = Cplx_CustomCNN_1D(input_shape=self.input_shape, num_classes=self.num_classes)
+        self.enc = Cplx_CustomCNN_1D(input_shape_custom=self.input_shape, num_classes=self.num_classes)
         self.attn = CustomAttentionLayer(units=64)
         self.classifier = CustomClassifierModel(num_classes=self.num_classes)
         
-    # encode the cluster with Cplx_CustomCNN_1D and return the "predictions"
     def calculate_input_shape(self):
-        cluster_centers = self.cluster_centers
+        self.kmeans.fit(pixels)
+        cluster_centers = self.kmeans.get_cluster_centers()
+        print("cluster_centers shape ",cluster_centers.shape)
         return (cluster_centers[0].reshape((1,) + cluster_centers[0].shape)).shape[1:]
     
     def build(self, learning_rate=0.001):
@@ -120,4 +36,26 @@ class MODEL():
         self.model.fit(train_data, train_labels, epochs=epochs, batch_size=batch_size)
         
         
+# Usage example, With an segment of 100 pixels, each with shape (73, 10)
+pixels = np.random.random((100, 73, 10))
+
+# we want to generate more realistic values for the pixels, so we help the clustering algorithm
+generator = PixelValueGenerator(low_value_percentage=0.8)
+generator.generate_values(pixels.shape)
+pixels = generator.get_values()
+print("pixels shape ",pixels.shape)
+
+# object of the class CustomKMeans
+n_clusters=2
+custom_kmeans = CustomKMeans(n_clusters=n_clusters)
+
+# Fit the K-Means model and retrieve cluster labels and centers
+custom_kmeans.fit(pixels)
+
+MODEL = MODEL(num_classes=7, num_clusters=2)
+
+MODEL.build(learning_rate=0.001)
+
+MODEL.train(pixels, custom_kmeans.get_cluster_labels(), epochs=10, batch_size=32)
+
  
--- py3/CNN_model.py
+++ py3/CNN_model.py
@@ -1,7 +1,7 @@
-from tensorflow import keras
+import tensorflow as tf
+from keras import layers
 
-     
-class Cplx_CustomCNN_1D:
+class Cplx_CustomCNN_1D(layers.Layer):
     """Custom 1D Convolutional Neural Network.
 
     Args:
@@ -12,49 +12,42 @@ class Cplx_CustomCNN_1D:
         input_shape (tuple): The input shape of the model.
         num_classes (int): The number of target classes.
         model (keras.Sequential): The built CNN model.
-
-    Methods:
-        build_model: Build the 1D CNN model.
-        compile_model: Compile the model with an optimizer and loss function.
-        summary: Display a summary of the model architecture.
     """
-    def __init__(self, input_shape=(730, 10), num_classes=7):
-        self.input_shape = input_shape
+    def __init__(self, input_shape_custom=(73, 10), num_classes=7):
+        super(Cplx_CustomCNN_1D, self).__init__()
+        self.input_shape_custom = input_shape_custom
         self.num_classes = num_classes
-        self.model = self.build_model()
 
-    def build_model(self):
-        model = keras.Sequential()#keras.model
-        
-        model.add(keras.layers.Conv1D(256, kernel_size=3, activation='relu', input_shape=self.input_shape))
-        model.add(keras.layers.BatchNormalization())
-        model.add(keras.layers.Dropout(0.4))
+        self.model = tf.keras.Sequential()
 
-        model.add(keras.layers.Conv1D(256, kernel_size=3, activation='relu'))
-        model.add(keras.layers.BatchNormalization())
-        model.add(keras.layers.Dropout(0.4))
+        self.model.add(layers.Conv1D(256, kernel_size=3, activation='relu', input_shape=self.input_shape_custom))
+        self.model.add(layers.BatchNormalization())
+        self.model.add(layers.Dropout(0.4))
 
-        model.add(keras.layers.Conv1D(256, kernel_size=3, activation='relu'))
-        model.add(keras.layers.BatchNormalization())
-        model.add(keras.layers.Dropout(0.4))
+        self.model.add(layers.Conv1D(256, kernel_size=3, activation='relu'))
+        self.model.add(layers.BatchNormalization())
+        self.model.add(layers.Dropout(0.4))
 
-        #model.add(keras.layers.MaxPooling1D(pool_size=2))  # MaxPooling au lieu de GlobalAveragePooling
-        
-        model.add(keras.layers.Conv1D(512, kernel_size=3, activation='relu'))
-        model.add(keras.layers.BatchNormalization())
-        model.add(keras.layers.Dropout(0.4))
+        self.model.add(layers.Conv1D(256, kernel_size=3, activation='relu'))
+        self.model.add(layers.BatchNormalization())
+        self.model.add(layers.Dropout(0.4))
 
-        model.add(keras.layers.Conv1D(512, kernel_size=1, activation='relu'))
-        model.add(keras.layers.BatchNormalization())
-        model.add(keras.layers.Dropout(0.4))
+        self.model.add(layers.Conv1D(512, kernel_size=3, activation='relu'))
+        self.model.add(layers.BatchNormalization())
+        self.model.add(layers.Dropout(0.4))
 
-        model.add(keras.layers.Conv1D(512, kernel_size=1, activation='relu'))
-        model.add(keras.layers.BatchNormalization())
-        model.add(keras.layers.Dropout(0.4))
+        self.model.add(layers.Conv1D(512, kernel_size=1, activation='relu'))
+        self.model.add(layers.BatchNormalization())
+        self.model.add(layers.Dropout(0.4))
 
-        model.add(keras.layers.GlobalAveragePooling1D()) 
-        
-        return model
+        self.model.add(layers.Conv1D(512, kernel_size=1, activation='relu'))
+        self.model.add(layers.BatchNormalization())
+        self.model.add(layers.Dropout(0.4))
+
+        self.model.add(layers.GlobalAveragePooling1D())
+
+    def call(self, inputs):
+        return self.model(inputs)
 
     def compile_model(self):
         self.model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
