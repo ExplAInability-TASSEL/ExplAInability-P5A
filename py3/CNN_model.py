6ae7043eb21280d3b82ddda725a93b9d4a6c0ec1
Thibault-GILLARD <thibault.gillard@epfedu.fr>
2023-12-07 11:50:38 +0100

    Lot of

--- py3/CNN_model.py
+++ py3/CNN_model.py
@@ -54,78 +54,31 @@ class Cplx_CustomCNN_1D(layers.Layer):
 
     def summary(self):
         self.model.summary()
+        
+        
+from efficientnet.keras import EfficientNetB0
 
-class CustomCNN:
+class New_CustomCNN1D(tf.keras.Model):
     def __init__(self, input_shape=(73, 10), num_classes=8):
-        self.input_shape = input_shape
-        self.num_classes = num_classes
-        self.model = self.build_model()
+        super(New_CustomCNN1D, self).__init__()
 
-    def build_model(self):
-        model = keras.Sequential()
-        
-        model.add(keras.layers.Conv2D(256, (3, 3), strides=1, activation='relu', input_shape=self.input_shape))#
-        model.add(keras.layers.BatchNormalization())
-        model.add(keras.layers.Dropout(0.4))
+        self.model = tf.keras.Sequential()
 
-        model.add(keras.layers.Conv2D(256, (3, 3), strides=1, activation='relu'))
-        model.add(keras.layers.BatchNormalization())
-        model.add(keras.layers.Dropout(0.4))
-
-        model.add(keras.layers.Conv2D(256, (3, 3), strides=1, activation='relu'))
-        model.add(keras.layers.BatchNormalization())
-        model.add(keras.layers.Dropout(0.4))
-
-        model.add(keras.layers.Conv2D(256, (3, 3), strides=2, activation='relu'))
-        model.add(keras.layers.BatchNormalization())
-        model.add(keras.layers.Dropout(0.4))
-
-        model.add(keras.layers.Conv2D(512, (3, 3), strides=1, activation='relu'))
-        model.add(keras.layers.BatchNormalization())
-        model.add(keras.layers.Dropout(0.4))
-
-        model.add(keras.layers.Conv2D(512, (1, 1), strides=1, activation='relu'))
-        model.add(keras.layers.BatchNormalization())
-        model.add(keras.layers.Dropout(0.4))
-
-        model.add(keras.layers.Conv2D(512, (1, 1), strides=1, activation='relu'))
-        model.add(keras.layers.BatchNormalization())
-        model.add(keras.layers.Dropout(0.4))
-
-        model.add(keras.layers.GlobalAveragePooling2D())
-        
-        model.add(keras.layers.Dense(self.num_classes, activation='softmax'))
-        
-        return model
+        self.model.add(layers.Conv1D(64, kernel_size=2, activation='relu', input_shape=input_shape))
+        self.model.add(layers.MaxPooling1D(pool_size=2))
+        self.model.add(layers.Conv1D(128, kernel_size=2, activation='relu'))
+        self.model.add(layers.MaxPooling1D(pool_size=2))
+        self.model.add(layers.Conv1D(256, kernel_size=2, activation='relu'))
+        self.model.add(layers.MaxPooling1D(pool_size=2))
+        self.model.add(layers.GlobalAveragePooling1D())
+        self.model.add(layers.Dense(512, activation='relu'))
+        self.model.add(layers.BatchNormalization())
+        self.model.add(layers.GlobalAveragePooling1D())
+    def call(self, inputs):
+        return self.model(inputs)
 
     def compile_model(self):
         self.model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
     def summary(self):
         self.model.summary()
-        
-class CustomCNN_1D:
-    def __init__(self, input_shape=(73, 10), num_classes=7):
-        self.input_shape = input_shape
-        self.num_classes = num_classes
-        self.model = self.build_model()
-
-    def build_model(self):
-        model = keras.Sequential()
-        
-        model.add(keras.layers.Conv1D(256, kernel_size=3, strides=1, activation='relu', input_shape=self.input_shape))
-        model.add(keras.layers.BatchNormalization())
-        model.add(keras.layers.Dropout(0.4))
-        
-        model.add(keras.layers.GlobalAveragePooling1D()) 
-        
-        model.add(keras.layers.Dense(self.num_classes, activation='softmax'))
-        
-        return model
-
-    def compile_model(self):
-        self.model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-
-    def summary(self):
-        self.model.summary()
-   
