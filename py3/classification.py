cf6484967c8b97551bfeb1e00fb3a07840a289d3
Thibault-GILLARD <thibault.gillard@epfedu.fr>
2023-12-21 19:41:24 +0100

    clean classification.py

--- py3/classification.py
+++ py3/classification.py
@@ -1,25 +1,29 @@
-from keras.layers import Layer, BatchNormalization, ReLU, Dense
+from keras.layers import Layer, BatchNormalization, ReLU, Dense, Dropout
 import tensorflow as tf
 
 class CustomFullyConnectedLayer(Layer):
-    def __init__(self, units, **kwargs):
+    def __init__(self, units, dropout_rate=0.5, **kwargs):
         super(CustomFullyConnectedLayer, self).__init__(**kwargs)
         self.units = units
+        self.dropout_rate = dropout_rate
 
     def build(self, input_shape):
         self.fc = Dense(self.units, activation=None)
         self.bn = BatchNormalization()
         self.relu = ReLU()
+        self.dropout = Dropout(self.dropout_rate)
         super(CustomFullyConnectedLayer, self).build(input_shape)
 
     def call(self, inputs):
         x = self.fc(inputs)
         x = self.bn(x)
         x = self.relu(x)
+        x = self.dropout(x)
         return x
 
     def compute_output_shape(self, input_shape):
         return input_shape
+    
 
 class CustomClassifierModel(tf.keras.Model):
     """
@@ -33,14 +37,14 @@ class CustomClassifierModel(tf.keras.Model):
         fc_units (int, optional): The number of units in the fully connected layers. Defaults to 512.
 
     """
-    def __init__(self, num_classes, fc_units=512, **kwargs):
+    def __init__(self, num_classes, fc_units=512, dropout_rate=0, **kwargs):
         super(CustomClassifierModel, self).__init__(**kwargs)
         self.num_classes = num_classes
         self.fc_units = fc_units
 
         # Define Fully Connected Layers
-        self.fc1 = CustomFullyConnectedLayer(units=fc_units)
-        self.fc2 = CustomFullyConnectedLayer(units=fc_units)
+        self.fc1 = CustomFullyConnectedLayer(units=fc_units, dropout_rate=dropout_rate)
+        self.fc2 = CustomFullyConnectedLayer(units=fc_units, dropout_rate=dropout_rate)
 
         # Output Layer
         self.output_layer = Dense(num_classes, activation='softmax')
@@ -53,5 +57,7 @@ class CustomClassifierModel(tf.keras.Model):
 
     def compute_output_shape(self, input_shape):
         return (input_shape[0], self.num_classes)
+    
+    
 
 
